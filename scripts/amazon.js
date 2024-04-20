a891331c128d2e216858ba28ac0ef9c5155981ae
Kelvin-Peprah <kelvinpeprah0@gmail.com>
2024-04-20 09:36:48 -0800

    second commit

---
+++ scripts/amazon.js
@@ -0,0 +1,15 @@
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090
+}];
+
+products.forEach((product) => {
+  `
+  
+  `
+});
