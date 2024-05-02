86ac31c7a75f5debbb6b46307dfb998d295b8311
Kelvin-Peprah <kelvinpeprah0@gmail.com>
2024-05-01 20:12:58 -0800

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
@@ -8,6 +8,10 @@ export let cart = [
     quantity: 1,
   },
 ];
+
+function saveTocart() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   // make the select list interactive
@@ -42,6 +46,8 @@ export function addToCart(productId) {
       quantity: itemSelector,
     });
   }
+
+  saveTocart();
 }
 
 export function removeFromCart(productId) {
@@ -53,4 +59,6 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+
+  saveTocart();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,9 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
       <div class="delivery-date">
         Delivery date: Wednesday, June 15
       </div>
@@ -99,6 +101,10 @@ document.querySelectorAll('.js-delete-link').forEach((deleteLink) => {
   deleteLink.addEventListener('click', () => {
     const productId = deleteLink.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    const removeCartContainer = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    removeCartContainer.remove();
+    console.log(removeCartContainer);
   });
 });
